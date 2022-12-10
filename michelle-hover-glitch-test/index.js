af449cdaaa851103ddae0e4ec4adb73ca4457fe6
Michelle <mliuu3259@gmail.com>
2022-12-10 13:22:48 -0500

    freeze images on hover

---
+++ michelle-hover-glitch-test/index.js
@@ -0,0 +1,47 @@
+const imgArr = document.querySelectorAll(".img");
+let prevX = 0;
+let prevY = 0;
+
+let movexAmount = 0;
+let moveyAmount = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mousePos(e);
+});
+
+const mousePos = (e) => {
+  movexAmount = e.pageX - prevX;
+  moveyAmount = e.pageY - prevY;
+
+  moveImg(movexAmount, moveyAmount);
+
+  prevX = e.pageX;
+  prevY = e.pageY;
+};
+
+const moveImg = (xAmount, yAmount) => {
+  console.log(xAmount, yAmount);
+  imgArr.forEach((img) => {
+    let movementStrength = 5 + Math.random() * 15;
+    img.style.left = img.offsetLeft - xAmount / movementStrength + "px";
+    img.style.top = img.offsetTop - yAmount / movementStrength + "px";
+  });
+};
+
+imgArr.forEach((img, index) => {
+  const imgAnimation = img.style.animation;
+  img.addEventListener("mouseover", () => {
+    imgArr.forEach((img, idx) => {
+      if (index !== idx) {
+        img.style.filter = "grayscale(1)";
+      }
+      img.style.animationPlayState = "paused";
+    });
+  });
+  img.addEventListener("mouseout", () => {
+    imgArr.forEach((img, idx) => {
+      img.style.filter = "none";
+      img.style.animationPlayState = "running";
+    });
+  });
+});
