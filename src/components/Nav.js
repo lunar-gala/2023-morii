2e44f0e76374312a84f7fd3101ac64de43aea58a
Michelle <mliuu3259@gmail.com>
2023-02-05 17:57:41 -0500

    nav list

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,6 +10,15 @@ export default function Nav({ page, setPage }) {
       <div className={cn(styles.border, styles.borderBottom)}></div>
       <div className={cn(styles.border, styles.borderLeft)}></div>
       <div className={cn(styles.border, styles.borderRight)}></div>
+      <div className={styles.navContainer}>
+        <ul className={styles.nav}>
+          <li>About</li>
+          <li>Lines</li>
+          <li>People</li>
+          <li>Watch</li>
+          <li>Tickets</li>
+        </ul>
+      </div>
     </div>
   );
 }
