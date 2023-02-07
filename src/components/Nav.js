b622e43dae35e212897e5a0adfe207e02de06dec
Michelle <mliuu3259@gmail.com>
2023-02-07 01:28:22 -0500

    fixed countdown load

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 
 import styles from './Nav.module.css';
@@ -8,30 +8,40 @@ import battery from '../assets/icons/battery.png';
 export default function Nav({ page, setPage }) {
   const countDownDate = new Date('Mar 18, 2023 19:00:00').getTime();
 
+  const getCountdown = () => {
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    return `${days}D ${hours}H ${minutes}H ${seconds}S`;
+  };
+
+  const [countdown, setCountdown] = useState(getCountdown());
+
   useEffect(() => {
     let x = setInterval(function () {
       // Get today's date and time
-      var now = new Date().getTime();
+      const now = new Date().getTime();
 
       // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // Output the result in an element with id="demo"
-      document.getElementById('countdown').innerHTML =
-        days + 'D ' + hours + 'H ' + minutes + 'H ' + seconds + 'S ';
+      const distance = countDownDate - now;
 
       // If the count down is over, write some text
       if (distance < 0) {
         clearInterval(x);
-        document.getElementById('countdown').innerHTML = 'THE MOMENT HAS FLED';
+        setCountdown('THE MOMENT HAS FLAD');
+      } else {
+        setCountdown(getCountdown());
       }
     }, 1000);
   }, []);
@@ -52,7 +62,7 @@ export default function Nav({ page, setPage }) {
       <div class={styles.topBar}>
         <p>24v</p>
         <p>RAW</p>
-        <p id="countdown"></p>
+        <p id="countdown">{countdown}</p>
         <img src={battery} alt="battery icon" />
         <p>72%</p>
       </div>
