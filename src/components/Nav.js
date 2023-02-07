d7b9dcd4915315ea9a58a2d99fe9e3c8665b9b08
Michelle <mliuu3259@gmail.com>
2023-02-07 01:19:58 -0500

    moved topbar to main

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import cn from 'classnames';
 
 import styles from './Nav.module.css';
 import { NAV } from '../assets/constants';
+import battery from '../assets/icons/battery.png';
 
 export default function Nav({ page, setPage }) {
+  const countDownDate = new Date('Mar 18, 2023 19:00:00').getTime();
+
+  useEffect(() => {
+    let x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      document.getElementById('countdown').innerHTML =
+        days + 'D ' + hours + 'H ' + minutes + 'H ' + seconds + 'S ';
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('countdown').innerHTML = 'THE MOMENT HAS FLED';
+      }
+    }, 1000);
+  }, []);
+
   return (
     <>
       <div className={cn(styles.border, styles.borderTop)}></div>
@@ -17,6 +48,13 @@ export default function Nav({ page, setPage }) {
             <li onClick={setPage(nav)}>{nav}</li>
           ))}
         </ul>
+      </div>
+      <div class={styles.topBar}>
+        <p>24v</p>
+        <p>RAW</p>
+        <p id="countdown"></p>
+        <img src={battery} alt="battery icon" />
+        <p>72%</p>
       </div>
     </>
   );
