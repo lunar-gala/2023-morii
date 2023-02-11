5d9fb0125e8e21a00977a4599c95e2ac47c81652
Michelle <mliuu3259@gmail.com>
2023-02-11 15:41:05 -0500

    refactoried story object

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,32 +1,39 @@
 export const STORY = [
-  {
-    text: 'In the moment, you start to feel it fading away.',
-    classes: ['center'],
-    screen: 0,
-  },
-  {
-    text: "Dancing in the shadows, whispering in the wind. A story that can't be contained is lost as one that can't be told.",
-    classes: ['center'],
-    screen: 1,
-  },
-  {
-    text: 'Allow me your time, for the sake of brevity. Hold still and look around. Try to remember. Try to keep me close.',
-    classes: ['center'],
-    screen: 2,
-  },
-  { text: '"How do I know this is real?"', classes: [], screen: 3 },
-  { text: '"You built it."', classes: [], screen: 3 },
-  { text: '"How will I know when to let go?"', classes: [], screen: 3 },
-  { text: '"I don\'t think you will."', classes: [], screen: 3 },
-  {
-    text: "We try to capture moments as if they'll escape, building a version of the world adherent to change. Can this memory be preserved, can this contour be casted?",
-    classes: [],
-    screen: 4,
-  },
-  {
-    text: '"What if we could just stay a little longer?"',
-    classes: [],
-    screen: 5,
-  },
-  { text: '"What if we never had to leave?"', classes: [], screen: 6 },
+  [
+    {
+      text: 'In the moment, you start to feel it fading away.',
+      classes: ['center'],
+    },
+  ],
+  [
+    {
+      text: "Dancing in the shadows, whispering in the wind. A story that can't be contained is lost as one that can't be told.",
+      classes: ['center'],
+    },
+  ],
+  [
+    {
+      text: 'Allow me your time, for the sake of brevity. Hold still and look around. Try to remember. Try to keep me close.',
+      classes: ['center'],
+    },
+  ],
+  [
+    { text: '"How do I know this is real?"', classes: [] },
+    { text: '"You built it."', classes: [] },
+    { text: '"How will I know when to let go?"', classes: [] },
+    { text: '"I don\'t think you will."', classes: [] },
+  ],
+  [
+    {
+      text: "We try to capture moments as if they'll escape, building a version of the world adherent to change. Can this memory be preserved, can this contour be casted?",
+      classes: [],
+    },
+  ],
+  [
+    {
+      text: '"What if we could just stay a little longer?"',
+      classes: [],
+    },
+  ],
+  [{ text: '"What if we never had to leave?"', classes: [] }],
 ];
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import styles from './Landing.module.css';
 import { STORY } from '../assets/constants';
 
 function Landing() {
-  const [storyNum, setStoryNum] = useState(3);
+  const updateStoryCount = () => {
+    console.log('storynum', storyNum, STORY.length);
+    if (storyNum >= STORY.length) setStoryNum(0);
+    else setStoryNum(storyNum + 1);
+  };
+
+  const [storyNum, setStoryNum] = useState(0);
 
   return (
-    <div className={styles.container}>
-      {[...Array(storyNum).keys()].map((storyNum) => {
-        return <Frame storyNum={storyNum} key={storyNum} />;
+    <div className={styles.container} onClick={updateStoryCount}>
+      {STORY.map((story, index) => {
+        return <Frame story={story} key={index} />;
       })}
     </div>
   );
 }
 
-function Frame({ storyNum }) {
-  const stories = STORY.filter(({ screen }) => parseInt(screen) === storyNum);
-  // console.log(STORY);
-
+function Frame({ story }) {
   return (
     <div className={styles.screen}>
-      {stories.map(({ text, classes }, index) => {
+      {story.map(({ text, classes }, index) => {
         return (
           <div className={styles.frame} key={index}>
             <p className={cn(...classes.map((c) => styles[c]))}>{text}</p>
