71bdb748c38744b12edf2010c522185da15c934f
Michelle <mliuu3259@gmail.com>
2023-02-11 12:01:29 -0500

    blur effect

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,36 @@
-// import styles from './Landing.css';
+import { useState } from 'react';
+import cn from 'classnames';
+
+import styles from './Landing.module.css';
+import { STORY } from '../assets/constants';
 
 function Landing() {
-  return <></>;
+  const [storyNum, setStoryNum] = useState(2);
+
+  return (
+    <div className={styles.container}>
+      {[...Array(storyNum).keys()].map((storyNum) => {
+        return <Frame storyNum={storyNum} key={storyNum} />;
+      })}
+    </div>
+  );
+}
+
+function Frame({ storyNum }) {
+  const stories = STORY.filter(({ screen }) => parseInt(screen) === storyNum);
+  // console.log(STORY);
+
+  return (
+    <div className={styles.screen}>
+      {stories.map(({ text, classes }, index) => {
+        return (
+          <div className={styles.frame} key={index}>
+            <p className={cn(...classes.map((c) => styles[c]))}>{text}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Landing;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 // import styles from './Home.module.css';
 
+import Landing from '../components/Landing';
+
 function Home() {
-  return <></>;
+  return <Landing />;
 }
 
 export default Home;
