f151490247fd8806ac77b3f8eb47ba79815971a6
yonmaor1 <yonmaor@cmu.edu>
2023-03-03 00:04:37 -0500

    implemented links

--- public/camera/sketch.js
+++ public/camera/sketch.js
@@ -6,27 +6,21 @@ let VIEWHEIGHT = viewport.clientHeight;
 let BG = '#C9C8C3';
 let bgImg;
 let focus;
-let lines = [];
+let LINES = [];
 
 let global_alpha = 0;
 
-let experiri;
-let active = 0;
-let hover = false;
-let curr_line = -1;
+let HOVER = false;
+let CURR_LINE = null;
 
 // line object //
-function create_line(limg, lnum) {
+function create_line(limg, lnum, llink) {
   var l = {
-    x: 0,
-    y: 0,
+    x: 0, y: 0,
     img: limg,
     num: lnum,
-    name:
-      'LINE ' +
-      lnum.toString() +
-      '  ' +
-      'DESIGNERS: JOHN SMITH, LUKE SKYWALKER, MITTENS THE CAT',
+    name: 'default',
+    link: llink,
     draw: draw_line,
     place: place_line,
     resize: resize_line,
@@ -72,7 +66,7 @@ function overlap_line() {
 function update_hover(lines_arr) {
   for (var i = 0; i < lines_arr.length; i++) {
     if (lines_arr[i].overlap()) {
-      hover = true;
+      HOVER = true;
 
       CURSOR.active = true;
       return i;
@@ -80,7 +74,7 @@ function update_hover(lines_arr) {
   }
 
   CURSOR.active = false;
-  hover = false;
+  HOVER = false;
 }
 
 // CURSOR //
@@ -106,14 +100,20 @@ function preload() {
   bgImg = loadImage('assets/lines-bg.png');
   focus = loadImage('icons/focus.png');
 
-  for (var i = 1; i <= 13; i++) {
-    let curr_img = loadImage('assets/lines' + i.toString() + '.png');
-    lines.push(create_line(curr_img, i));
+  let line_names = [ "arriba", "atrophy", "delicacy", "doodles", "kalopsia", 
+                      "lapinata", "limbic", "noxmemoria", "paperdollls", 
+                      "rewind", "secoulth", "weilai", "xiaoshi"];
+
+  for (var i = 0; i < line_names.length; i++) {
+    print(line_names[i]);
+    let curr_img = loadImage('assets/' + line_names[i] + '.png');
+    let curr_link = "./" + line_names[i] + '.html'; // probably needs changing
+    LINES.push(create_line(curr_img, i, curr_link));
   }
 
-  print(lines);
+  print(LINES);
 
-  experiri = loadImage('assets/experiri.png');
+  // experiri = loadImage('assets/experiri.png');
 
   // cursor
   let inner = loadImage('cursor/assets/moR_inner.png');
@@ -130,27 +130,27 @@ function setup() {
   textAlign(RIGHT, BOTTOM);
 
   for (var i = 0; i < 13; i++) {
-    lines[i].resize(0.6, 0);
+    LINES[i].resize(0.6, 0);
   }
 
   let xc = width/2671;
   let yc = height/1728;
 
-  lines[0].place(xc*131, yc*386);
-  lines[1].place(xc*386, yc*213);
-  lines[2].place(xc*456, yc*586);
-  lines[3].place(xc*168, yc*1200);
-  lines[4].place(xc*903, yc*70);
-  lines[5].place(xc*888, yc*375);
-  lines[6].place(xc*833, yc*927);
-  lines[7].place(xc*1243, yc*1005);
-  lines[8].place(xc*1381, yc*1333);
-  lines[9].place(xc*1935, yc*98);
-  lines[10].place(xc*1599, yc*277);
-  lines[11].place(xc*1739, yc*685);
-  lines[12].place(xc*2090, yc*1009);
-  // lines[13].place(900, 300);
-  // lines[14].place(1000, 580);
+  LINES[0].place(xc*131, yc*386);
+  LINES[1].place(xc*386, yc*213);
+  LINES[2].place(xc*456, yc*586);
+  LINES[3].place(xc*168, yc*1200);
+  LINES[4].place(xc*903, yc*70);
+  LINES[5].place(xc*888, yc*375);
+  LINES[6].place(xc*833, yc*927);
+  LINES[7].place(xc*1243, yc*1005);
+  LINES[8].place(xc*1381, yc*1333);
+  LINES[9].place(xc*1935, yc*98);
+  LINES[10].place(xc*1599, yc*277);
+  LINES[11].place(xc*1739, yc*685);
+  LINES[12].place(xc*2090, yc*1009);
+  // LINES[13].place(900, 300);
+  // LINES[14].place(1000, 580);
 
   CURSOR = make_cursor(width/2, height/2, IMAGES);
 }
@@ -176,72 +176,21 @@ function lines_page() {
   bgImg.resize(2 * width, 2 * height);
   image(bgImg, width / 2, height / 2);
 
-  image(lines[0].img, lines[0].x, lines[0].y);
-
-  lines[0].draw();
-  lines[1].draw();
-  lines[2].draw();
-  lines[3].draw();
-  lines[4].draw();
-  lines[5].draw();
-  lines[6].draw();
-  lines[7].draw();
-  lines[8].draw();
-  lines[9].draw();
-  lines[10].draw();
-  lines[11].draw();
-  lines[12].draw();
-  // lines[13].draw();
-  // lines[14].draw();
+  for (let i = 0; i < LINES.length; i++){
+    LINES[i].draw();
+  }
 
   pop();
 
   CURSOR.move();
   CURSOR.draw();
 
-  // // focus
-  // push();
-  // translate(width/2, height/2);
+  CURR_LINE = LINES[update_hover(LINES)];
 
-  // strokeWeight(2);
-  // stroke('black');
-
-  curr_line = update_hover(lines);
-  // if (hover){
-  //   stroke('black');
-  //   fill('white');
-  //   textSize(18);
-  //   textFont('monospace');
-  //   text(lines[curr_line].name, width/2 - 5, height/2 - 5);
-
-  //   stroke('red');
-  // }
-
-  // line(-40, 0, 40, 0);
-  // line(0, -40, 0, 40);
-
-  // stroke('black');
-  // for (var i = 0; i < 4; i++){
-  //   rotate(90);
-  //   line(-140, -140, -140 + 80, -140);
-  //   line(-140, -140, -140, -140 + 80);
-  // }
-
-  // pop();
-
-}
-
-function single_line_page() {
-  if (!curr_line || curr_line <= 0) return;
-  var myCustomData = { line: curr_line };
-  var event = new CustomEvent('single_line', { detail: myCustomData });
-  window.parent.document.dispatchEvent(event);
-
-  // experiri.resize(0.95 * width, 0.95 * height);
-  // image(experiri, width / 2, height / 2);
 }
 
 function mousePressed() {
-  print('pressed');
-  single_line_page();
+  if (CURSOR.active){
+    window.open(CURR_LINE.link, "_self");
+  }
 }
