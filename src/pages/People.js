24c3a2ff08a2c97e49735d210ef216d662975e13
Michelle <mliuu3259@gmail.com>
2023-03-16 18:13:41 -0400

    imported in peoples page, deleted people mobile page from public, created react People.js page

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,7 +1,17 @@
-// import styles from './People.module.css';
+import styles from './People.module.css';
 
-function People() {
-  return <></>;
+function People({ setCursor }) {
+  return (
+    <>
+      <div
+        id="viewport"
+        onMouseLeave={() => setCursor(true)}
+        onMouseOver={() => setCursor(false)}
+      >
+        <iframe src="/people-page/index.html" title="Lines Page"></iframe>
+      </div>
+    </>
+  );
 }
 
 export default People;
