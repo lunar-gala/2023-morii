d524095820fa050110fd95ea3d9de77299ff4b39
Michelle <mliuu3259@gmail.com>
2023-03-18 13:14:17 -0400

    wei lai & delicacy filter link

--- src/pages/Filter.js
+++ src/pages/Filter.js
@@ -2,14 +2,37 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { FILTERS } from '../assets/lines';
+import styles from './Filter.module.css';
+import delicacy from '../assets/delicacy_icon.jpg';
+import wei_lai from '../assets/weilai_icon.jpg';
 
 function Filter() {
   const { id } = useParams();
 
   useEffect(() => {
-    window.location.href = FILTERS[id];
+    if (!(id === 'wei_lai' || id === 'delicacy'))
+      window.location.href = FILTERS[id];
   }, []);
-  return <></>;
+
+  return (
+    <>
+      {id === 'wei_lai' || id === 'delicacy' ? (
+        <div className={styles.container}>
+          <p className={styles.select}>Select to activate filter</p>
+          <a href={FILTERS.wei_lai} className={styles.link}>
+            <img src={wei_lai} alt="delicacy icon" />
+            <p>Wei•Lai</p>
+          </a>
+          <a href={FILTERS.delicacy} className={styles.link}>
+            <img src={delicacy} alt="delicacy icon" />
+            <p>Delicacy</p>
+          </a>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default Filter;
