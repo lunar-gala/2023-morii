0d8eb32b24e00c504da4b66dca9d05b536c0eca2
Michelle <mliuu3259@gmail.com>
2023-04-23 10:51:48 -0400

    Updates

---
+++ camera/sketch.js
@@ -0,0 +1,328 @@
+let cnv;
+let viewport = document.getElementById('viewport');
+let VIEWWIDTH = viewport.clientWidth;
+let VIEWHEIGHT = viewport.clientHeight;
+
+let BG = '#F4F3EF';
+let bgImg;
+let focus;
+let LINES = [];
+
+let global_alpha = 0;
+
+let HOVER = false;
+let CURR_LINE = null;
+
+let isenheim;
+
+function abs_constrain(val, min, max) {
+  if (min < max) {
+    return constrain(val, min, max);
+  } else {
+    return constrain(val, max, min);
+  }
+}
+
+function first_letter_cap(str) {
+
+  if (str.toUpperCase() == "WEILAI") return "WeiLai";
+  if (str.toUpperCase() == "XIAOSHI") return "XiaoShi";
+
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+// line object //
+function create_line(limg, lnum, lname, fname) {
+  var l = {
+    x_target: 0,
+    y_target: 0,
+    x_actual: 0,
+    x_actual: 0,
+    text_x: random(-25, 25),
+    text_y: random(-100, 100), // magic numbers cuz p5 is weird
+    img: limg,
+    num: lnum,
+    name: lname,
+    display_name: fname,
+    rpx: null,
+    rpy: null,
+    draw: draw_line,
+    move: move_line,
+    place: place_line,
+    resize: resize_line,
+    overlap: overlap_line,
+  };
+
+  return l;
+}
+
+function draw_line() {
+  image(this.img, this.x_actual, this.y_actual);
+  if (this.overlap()) {
+    fill('black');
+    textFont(isenheim);
+    textSize(36);
+    text(
+      first_letter_cap(this.display_name),
+      this.x_actual + this.text_x,
+      this.y_actual + this.text_y
+    );
+  }
+}
+
+function move_line(rpx, rpy) {
+  let rpc = 5000; // repulsion force
+
+  if (rpx == null && this.wait <= 20) {
+    //groups are moving back to normal positions
+    this.wait++;
+
+    let dp = dist(this.x_actual, this.y_actual, this.rpx, this.rpy);
+
+    if (dp != 0) {
+      let f = rpc / dp;
+      let dirx = (this.x_actual - this.rpx) / dp;
+      let diry = (this.y_actual - this.rpy) / dp;
+      this.x_actual -= dirx * f;
+      this.y_actual -= diry * f;
+
+      this.x_actual = abs_constrain(
+        this.x_actual,
+        this.x_target,
+        this.x_target + (rpc / 500) * dirx * f
+      );
+      this.y_actual = abs_constrain(
+        this.y_actual,
+        this.y_target,
+        this.y_target + (rpc / 500) * diry * f
+      );
+    }
+  } else if (rpx != null) {
+    //groups are in / moving to repelled positions
+    this.wait = 0;
+
+    this.rpx = rpx;
+    this.rpy = rpy;
+
+    let dp = dist(this.x_actual, this.y_actual, rpx, rpy);
+
+    if (dp != 0) {
+      let f = rpc / dp;
+      let dirx = (this.x_actual - rpx) / dp;
+      let diry = (this.y_actual - rpy) / dp;
+      this.x_actual += dirx * f;
+      this.y_actual += diry * f;
+
+      this.x_actual = abs_constrain(
+        this.x_actual,
+        this.x_target,
+        this.x_target + (rpc / 500) * dirx * f
+      );
+      this.y_actual = abs_constrain(
+        this.y_actual,
+        this.y_target,
+        this.y_target + (rpc / 500) * diry * f
+      );
+    }
+  }
+}
+
+function place_line(lx, ly) {
+  this.x_target = lx;
+  this.y_target = ly;
+  this.x_actual = lx;
+  this.y_actual = ly;
+}
+
+function display_line(line_key) {
+  console.log('hi');
+  var myCustomData = { line: line_key };
+  var event = new CustomEvent('single_line', { detail: myCustomData });
+  window.parent.document.dispatchEvent(event);
+}
+
+function resize_line(sx, sy) {
+  this.img.resize(this.img.width * sx, this.img.height * sy);
+}
+
+function overlap_line() {
+  let centeredX = width / 2 - mouseX;
+  let centeredY = height / 2 - mouseY;
+
+  let lineX_lower = this.x_actual - this.img.width / 2 - width / 2;
+  let lineX_upper = this.x_actual + this.img.width / 2 - width / 2;
+  let lineY_lower = this.y_actual - this.img.height / 2 - height / 2;
+  let lineY_upper = this.y_actual + this.img.height / 2 - height / 2;
+  if (
+    lineX_lower < centeredX &&
+    centeredX < lineX_upper &&
+    lineY_lower < centeredY &&
+    centeredY < lineY_upper
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function update_hover(lines_arr) {
+  for (let i = 0; i < lines_arr.length; i++) {
+    if (lines_arr[i].overlap()) {
+      HOVER = true;
+
+      CURSOR.active = true;
+      return i;
+    }
+  }
+
+  CURSOR.active = false;
+  HOVER = false;
+}
+
+// CURSOR //
+
+let IMAGES = [];
+let CURSOR;
+
+// API
+
+// corner_t make_corner(cx, cy, cimg)
+
+// void corner_draw()
+
+// cursor_t make_cursor(cx, cy, cr, corner_arr)
+
+// void cursor_move()
+
+// void cursor_draw()
+
+//////////////////
+
+function preload() {
+  bgImg = loadImage('assets/lines-bg-new.png');
+  focus = loadImage('icons/focus.png');
+  isenheim = loadFont('assets/fonts/Isenheim_Regulier.otf');
+
+  let line_names = [
+    'arriba',
+    'atrophy',
+    'delicacy',
+    'doodles',
+    'kalopsia',
+    'lapinata',
+    'limbic',
+    'noxmemoria',
+    'paperdolls',
+    'rewind',
+    'selcouth',
+    'weilai',
+    'xiaoshi',
+  ];
+  let full_line_names = [
+    'Arriba de Monte Teide',
+    'Atrophy',
+    'Delicacy',
+    'Doodle',
+    'Kalopsia',
+    'La Piñata',
+    'Limbic',
+    'Nox Memoria',
+    'Paper Dolls',
+    'Rewind',
+    'Selcouth',
+    'Wei•Lai',
+    'Xiao•Shi',
+  ];
+
+  for (var i = 0; i < line_names.length; i++) {
+    // print(line_names[i]);
+    let curr_img = loadImage('assets/' + line_names[i] + '.png');
+    LINES.push(create_line(curr_img, i, line_names[i], full_line_names[i]));
+  }
+
+  // print(LINES);
+
+  // experiri = loadImage('assets/experiri.png');
+
+  // cursor
+  let inner = loadImage('cursor/assets/moR_inner.png');
+  let outer = loadImage('cursor/assets/moR_outer.png');
+  IMAGES = [inner, outer];
+}
+
+function setup() {
+  cnv = createCanvas(VIEWWIDTH, VIEWHEIGHT);
+  background(220, 100);
+  imageMode(CENTER);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  textAlign(CENTER, CENTER);
+
+  for (var i = 0; i < 13; i++) {
+    LINES[i].resize(0.8, 0);
+  }
+
+  let xc = width / 2671;
+  let yc = height / 1728;
+
+  LINES[0].place(xc * 590, yc * 1300);
+  LINES[1].place(xc * 386, yc * 213);
+  LINES[2].place(xc * 456, yc * 586);
+  LINES[3].place(xc * 380, yc * 1100);
+  LINES[4].place(xc * 903, yc * 70);
+  LINES[5].place(xc * 888, yc * 375);
+  LINES[6].place(xc * 833, yc * 927);
+  LINES[7].place(xc * 1243, yc * 1005);
+  LINES[8].place(xc * 1381, yc * 1333);
+  LINES[9].place(xc * 1935, yc * 98);
+  LINES[10].place(xc * 1599, yc * 277);
+  LINES[11].place(xc * 1739, yc * 685);
+  LINES[12].place(xc * 2090, yc * 1009);
+  // LINES[13].place(900, 300);
+  // LINES[14].place(1000, 580);
+
+  CURSOR = make_cursor(width / 2, height / 2, IMAGES);
+}
+
+function draw() {
+  cnv.parent('viewport');
+  background(BG);
+
+  let rpx = null;
+  let rpy = null;
+
+  if (CURR_LINE != null) {
+    rpx = CURR_LINE.x_actual;
+    rpy = CURR_LINE.y_actual;
+  }
+
+  noCursor();
+
+  push();
+  translate(mouseX - width / 2, mouseY - height / 2);
+  bgImg.resize(2 * width, 2 * height);
+  image(bgImg, width / 2, height / 2);
+
+  for (let i = 0; i < LINES.length; i++) {
+    LINES[i].draw();
+    LINES[i].move(rpx, rpy);
+  }
+
+  pop();
+
+  CURSOR.move();
+  CURSOR.draw();
+
+  CURR_LINE = LINES[update_hover(LINES)];
+}
+
+function mousePressed() {
+  if (CURSOR.active) {
+    // window.open(CURR_LINE.link, "_self");
+    display_line(CURR_LINE.name);
+  }
+}
---
+++ people-page/group.js
@@ -0,0 +1,282 @@
+function abs_constrain(val, min, max) {
+  if (min < max) {
+    return constrain(val, min, max);
+  } else {
+    return constrain(val, max, min);
+  }
+}
+
+function nth_letter_cap(str, index) {
+    return str.slice(0, index) + str[index].toUpperCase() + str.slice(index+1, str.length);
+}
+
+function first_letter_cap(str) {
+  if (str == 'PR') return str;
+  if (str == 'PR HEAD') return 'PR Head';
+
+  str = str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
+    if (str.slice(0, 3) == "Co-"){
+        str = nth_letter_cap(str, 3)
+    }
+
+  return str;
+}
+
+// header_t //
+function make_header() {
+  let h = {
+    parent: null,
+    children: null,
+    draw: draw_header,
+    overlap: overlap_header,
+    update: update_header,
+    resize: resize_header,
+  };
+
+  return h;
+}
+
+function draw_header() {
+  for (let i = 0; i < this.children.length; i++) {
+    this.children[i].draw();
+  }
+}
+
+function overlap_header() {
+  return;
+}
+
+function update_header() {
+  return;
+}
+
+function resize_header(rx, ry) {
+  return;
+}
+
+// group_t //
+function make_group(gparent, gname, grole, gimg, gisperson) {
+  let gx_origin;
+  let gy_origin;
+
+  if (gparent == null) {
+    gx_origin = 0;
+    gy_origin = 0;
+  } else {
+    gx_origin = gparent.x_target;
+    gy_origin = gparent.y_target;
+  }
+
+  let imgW = 100;
+  let imgH = 300;
+
+  let coheads = [
+    'JESSICA LAI',
+    'OLIVIA LUK',
+    'SHANNON LEE',
+    'CHRISTY ZO',
+    'CAMILLE CHANDLER',
+    'ALANA WU',
+    'SOWANG KUNDELING',
+    'ELLA SANAFILLIPO',
+  ];
+
+  let heads = [
+    'HEYSU OH',
+    'SABRINA XU',
+    'VIVIAN LEE',
+    'CODY RONG',
+    'MORGAN BOYD',
+    'SABINA SPEKTOR',
+    'OSCAR MONARREZ',
+    'PEIRAN GE',
+  ];
+
+  if (grole == 'PRODUCERS') {
+    grole = 'PRODUCER';
+
+    if (gname.toUpperCase() == 'NATHAN WAI') {
+      grole += ', MODEL';
+    }
+  } else if (coheads.includes(gname.toUpperCase())) {
+    grole = grole.replace('', 'CO-');
+    grole += ' HEAD';
+  } else if (heads.includes(gname.toUpperCase())) {
+    grole += ' HEAD';
+  } else if (gname.toUpperCase() == 'ANITA CHENG') {
+    grole += ', FINANCE HEAD';
+  }
+
+  let g = {
+    x_target: random(150 + imgW / 2, windowWidth - 234 - imgW / 2),
+    y_target: random(67 + imgH / 2, windowHeight - 34 - imgH / 2),
+    x_origin: gx_origin,
+    y_origin: gy_origin,
+    x_actual: gx_origin,
+    y_actual: gy_origin, // initially at origin
+    text_x: random(0, imgW),
+    text_y: random(0, imgW),
+    speed: 15, // inverse whoops
+    img: gimg,
+    name: gname,
+    role: grole,
+    alpha: 0,
+    text_alpha: 0,
+    parent: gparent, //group_t
+    children: [], //group_t[]
+    active: false,
+    draw: draw_group,
+    move: move_group,
+    resize: resize_group,
+    overlap: overlap_group,
+    isperson: gisperson, // for recursion
+    wait: 0,
+    rpx: null,
+    rpy: null,
+  };
+
+  return g;
+}
+
+function draw_group() {
+  push();
+
+  translate(this.x_actual, this.y_actual);
+  tint(255, this.alpha);
+  image(this.img, 0, 0);
+
+  noStroke();
+  fill(0, this.alpha);
+  textFont(isenheim);
+
+  let line_spacing;
+  if (this.isperson) {
+    line_spacing = 22;
+    textSize(18);
+  } else if (!this.isperson) {
+    line_spacing = 40;
+    textSize(36);
+  }
+  fill(0, this.text_alpha);
+  strokeWeight(0.5);
+
+  if (this.isperson) stroke(0, this.text_alpha);
+  text(first_letter_cap(this.name), this.text_x, this.text_y);
+  noStroke();
+  text(first_letter_cap(this.role), this.text_x, this.text_y + line_spacing);
+
+  pop();
+
+  // for (let i = 0; i < this.children.length; i++){
+  //     this.children[i].draw();
+  // }
+}
+
+function move_group(rpx, rpy) {
+  let dx = (this.x_target - this.x_origin) / this.speed;
+  let dy = (this.y_target - this.y_origin) / this.speed;
+
+  if (this.parent.active) dir = 1;
+  else dir = -1;
+
+  let rpc = 5000; // repulsion force
+
+  if (rpx == null && this.wait > 20) {
+    //groups are in normal positions
+    this.wait++;
+
+    this.x_actual += dir * dx;
+    this.y_actual += dir * dy;
+    this.alpha += (dir * 255) / this.speed;
+
+    this.x_actual = abs_constrain(this.x_actual, this.x_origin, this.x_target);
+    this.y_actual = abs_constrain(this.y_actual, this.y_origin, this.y_target);
+  } else if (rpx == null && this.wait <= 20) {
+    //groups are moving back to normal positions
+    this.wait++;
+
+    let dp = dist(this.x_actual, this.y_actual, this.rpx, this.rpy);
+    this.alpha += (dir * 255) / this.speed;
+
+    if (dp != 0) {
+      let f = (10 * rpc) / (dp * this.speed);
+      let dirx = (this.x_actual - this.rpx) / dp;
+      let diry = (this.y_actual - this.rpy) / dp;
+      this.x_actual -= dirx * f;
+      this.y_actual -= diry * f;
+
+      this.x_actual = abs_constrain(
+        this.x_actual,
+        this.x_target,
+        this.x_target + (rpc / 250) * dirx * f
+      );
+      this.y_actual = abs_constrain(
+        this.y_actual,
+        this.y_target,
+        this.y_target + (rpc / 250) * diry * f
+      );
+    }
+  } else if (rpx != null) {
+    //groups are in / moving to repelled positions
+    this.wait = 0;
+
+    this.rpx = rpx;
+    this.rpy = rpy;
+
+    let dp = dist(this.x_actual, this.y_actual, rpx, rpy);
+
+    if (dp != 0) {
+      let f = (10 * rpc) / (dp * this.speed);
+      let dirx = (this.x_actual - rpx) / dp;
+      let diry = (this.y_actual - rpy) / dp;
+      this.x_actual += dirx * f;
+      this.y_actual += diry * f;
+
+      this.x_actual = abs_constrain(
+        this.x_actual,
+        this.x_target,
+        this.x_target + (rpc / 250) * dirx * f
+      );
+      this.y_actual = abs_constrain(
+        this.y_actual,
+        this.y_target,
+        this.y_target + (rpc / 250) * diry * f
+      );
+    }
+  }
+
+  this.alpha = constrain(this.alpha, 0, 255);
+
+  for (let i = 0; i < this.children.length; i++) {
+    this.children[i].move();
+  }
+}
+
+function resize_group(sx, sy) {
+  this.img.resize(this.img.width * sx, this.img.height * sy);
+}
+
+function overlap_group() {
+  let centeredX = width / 2 - mouseX;
+  let centeredY = height / 2 - mouseY;
+
+  let lineX_lower = this.x_actual - this.img.width / 2 - width / 2;
+  let lineX_upper = this.x_actual + this.img.width / 2 - width / 2;
+  let lineY_lower = this.y_actual - this.img.height / 2 - height / 2;
+  let lineY_upper = this.y_actual + this.img.height / 2 - height / 2;
+  if (
+    lineX_lower < centeredX &&
+    centeredX < lineX_upper &&
+    lineY_lower < centeredY &&
+    centeredY < lineY_upper
+  ) {
+    return true;
+  }
+
+  return false;
+}
